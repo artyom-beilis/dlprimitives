bee34398524813e7ae87f2fade5b1cddde751157
Artyom Beilis <artyomtnk@yahoo.com>
2021-06-05 13:52:25 +0300

    More improved performnace benchmarks

--- tools/bench_openvino.py
+++ tools/bench_openvino.py
@@ -2,13 +2,10 @@ import os
 import json
 import argparse
 import time
-import plaidml
-import onnx
 import sys
-import onnx_plaidml
-import onnx_plaidml.backend
 import numpy as np
 from PIL import Image
+from openvino.inference_engine import IECore
 
 
 def predict_on_images(model,images,config):
@@ -33,7 +30,7 @@ def predict_on_images(model,images,config):
         image = np.zeros((1,3,th,tw),dtype=np.float32)
         for k in range(3):
             image[0,k,:,:] = npimg[dr:dr+th,dc:dc+tw,k] * fact[k] + off[k]
-        res = model.run([image])[0]
+        res = model(image)
         index = np.argmax(res[0]).item()
         csv.append([path,str(index),classes[index]] + ['%8.6f' % v for v in res[0].tolist()])
     with open('report.csv','w') as f:
@@ -51,7 +48,7 @@ def benchmark_model(model,batch,warm,iters):
     print("Warming up")
     for it in range(-warm,iters):
         start = time.time()
-        res = model.run([inp])[0]
+        res = model(inp)
         end = time.time()
         msg = ''
         if it == -warm:
@@ -74,20 +71,29 @@ def get_config():
     return cfg
 
 
-def get_model(path):
-    plaidml.settings.prohibit_winograd=False
+class VinoModel(object):
+    def __init__(self,path,device):
+        self.ie = IECore()
+        self.net = self.ie.read_network(model=path)
+        assert len(self.net.input_info) == 1 and len(self.net.outputs) == 1
+        self.input_blob = next(iter(self.net.input_info))
+        self.out_blob = next(iter(self.net.outputs))
+        self.net.input_info[self.input_blob].precision = 'FP32'
+        self.net.outputs[self.out_blob].precision = 'FP32'
+        assert self.net.outputs[self.out_blob].shape[1] == 1000
+        self.exec_net = self.ie.load_network(network = self.net,device_name = device)
 
-    model = onnx.load(path)
+    def __call__(self,images):
+        return self.exec_net.infer(inputs = {self.input_blob:images})['prob']
 
-    rep = onnx_plaidml.backend.prepare(model)
-    return rep
 
-    inp = np.random.random((16,3,224,224)).astype(np.float32)
+def get_model(path,dev):
+    model = VinoModel(path,dev)
+    return model
 
-    res = rep.run([inp])
 
 def main(args):
-    m=get_model(args.model)
+    m=get_model(args.model,args.device)
 
     if args.benchmark:
         benchmark_model(m,args.batch,args.warm,args.iters)
@@ -97,6 +103,7 @@ def main(args):
 if __name__ == '__main__': 
     p = argparse.ArgumentParser()
     p.add_argument('--model',default='vgg16')
+    p.add_argument('--device',default='GPU')
     p.add_argument('--benchmark',action='store_true')
     p.add_argument('--batch',default=16,type=int)
     p.add_argument('--warm',default=5,type=int)
