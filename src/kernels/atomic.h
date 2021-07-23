8559e6aae8f682e7fdb71379f49aef9b9db4d6fc
Artyom Beilis <artyomtnk@yahoo.com>
2021-07-23 21:51:08 +0300

    Performance issue with atomics fix

--- src/kernels/atomic.h
+++ src/kernels/atomic.h
@@ -1,5 +1,13 @@
 void atomic_addf(__global volatile float *ptr,float v)
 {
+#if defined(cl_intel_subgroups)
+    __global atomic_int *p = (__global atomic_int *)(ptr);
+    int prev,newv;
+    do {
+        prev = atomic_load(p);
+        newv = as_int(as_float(prev) + v);
+    } while(! atomic_compare_exchange_weak(p,&prev,newv));
+#else
     float oldv = *ptr;
     for(;;) {
         float newv = oldv + v;
@@ -8,5 +16,6 @@ void atomic_addf(__global volatile float *ptr,float v)
             return;
         oldv = prev;
     }
+#endif    
 }
 
