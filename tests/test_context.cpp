0e591a5d84e214b9c1c4997848d38a320e3366f5
Artyom Beilis <artyomtnk@yahoo.com>
2021-08-29 12:02:56 +0300

    Split dlprim to dlprim_core and dlprim libraries

--- tests/test_context.cpp
+++ tests/test_context.cpp
@@ -2,6 +2,7 @@
 #include <dlprim/tensor.hpp>
 #include <dlprim/gpu/program_cache.hpp>
 #include <iostream>
+#include "test.hpp"
 
 namespace dp = dlprim;
 int main(int argc,char **argv)
@@ -16,7 +17,7 @@ int main(int argc,char **argv)
         std::cerr << ctx.name() << std::endl;
         dp::Tensor a(ctx,dp::Shape(10));
 
-        cl::CommandQueue q(ctx.context(),ctx.device());
+        dp::ExecutionContext q = ctx.make_execution_context();
     
 
         float *p = a.data<float>();
@@ -25,22 +26,22 @@ int main(int argc,char **argv)
             p[i] = -5.0 + i;
         a.to_device(q);
         cl::Program const &prg = dp::gpu::Cache::instance().get_program(ctx,"bias","ACTIVATION",int(dp::StandardActivations::relu));
-        //cl::Program const &prg = dp::gpu::Cache::build_program(ctx,"bias","ACTIVATION",int(dp::StandardActivations::relu));
         cl::Kernel k(prg,"activation_inplace");
         int pos=0;
         k.setArg(pos++,int(a.shape().total_size()));
         a.set_arg(k,pos);
-        q.enqueueNDRangeKernel(k,cl::NullRange,cl::NDRange(a.shape().total_size()),cl::NullRange,nullptr,nullptr);
+        q.queue().enqueueNDRangeKernel(k,cl::NullRange,cl::NDRange(a.shape().total_size()),cl::NullRange,nullptr,nullptr);
         a.to_host(q,false);
         q.finish();
         for(unsigned i=0;i<a.shape()[0];i++) {
-            std::cerr << p[i] << std::endl;
+            TEST(p[i] == std::max(0.0,-5.0 + i));
         }
+        std::cout << "Ok" << std::endl;
     }
     catch(std::exception const &e) {
         std::cerr <<"Failed:"<< e.what() << std::endl;
+        return 1;
     }
-
     return 0;
 
 }
