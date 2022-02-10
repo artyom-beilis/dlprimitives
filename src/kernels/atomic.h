6a34a58755300bb24bffc76aac611a119288436b
Artyom Beilis <artyomtnk@yahoo.com>
2022-02-10 23:52:59 +0200

    Fixed incorrect atomic operation - you can't relay on v==v in floats since NaN == NaN is always false

--- src/kernels/atomic.h
+++ src/kernels/atomic.h
@@ -11,10 +11,10 @@ void atomic_addf(__global volatile float *ptr,float v)
     float oldv = *ptr;
     for(;;) {
         float newv = oldv + v;
-        float prev = as_float(atomic_cmpxchg((__global volatile int *)(ptr),as_int(oldv),as_int(newv)));
-        if(prev == oldv)
+        int prev = atomic_cmpxchg((__global volatile int *)(ptr),as_int(oldv),as_int(newv));
+        if(prev == as_int(oldv))
             return;
-        oldv = prev;
+        oldv = as_float(prev);
     }
 #endif    
 }
