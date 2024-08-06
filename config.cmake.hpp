5014276927f0945e82a35a99900b3874f443811d
Artyom Beilis <artyomtnk@yahoo.com>
2024-08-06 15:03:29 +0300

    Moving to new headers opencl.hpp

--- config.cmake.hpp
+++ config.cmake.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #cmakedefine DLPRIM_USE_CL1_HPP
+#cmakedefine DLPRIM_USE_CL2_HPP
 #cmakedefine WITH_SQLITE3
 #cmakedefine WITH_ONNX
 #cmakedefine WITH_CAFFE
--- include/dlprim/opencl_include.hpp
+++ include/dlprim/opencl_include.hpp
@@ -7,7 +7,25 @@
 ///////////////////////////////////////////////////////////////////////////////
 #pragma once
 #include <dlprim/config.hpp>
-#ifndef DLPRIM_USE_CL1_HPP
+#if !defined(DLPRIM_USE_CL1_HPP) && !defined(DLPRIM_USE_CL2_HPP)
+#  ifndef CL_HPP_ENABLE_EXCEPTIONS
+#     define CL_HPP_ENABLE_EXCEPTIONS
+#  endif
+#  ifndef CL_HPP_MINIMUM_OPENCL_VERSION
+#    define CL_HPP_MINIMUM_OPENCL_VERSION 120
+#  endif
+#  ifndef CL_TARGET_OPENCL_VERSION
+#    define CL_TARGET_OPENCL_VERSION 120
+#  endif
+#  ifndef CL_HPP_TARGET_OPENCL_VERSION
+#    define CL_HPP_TARGET_OPENCL_VERSION 120
+#  endif
+#  ifdef __APPLE__
+#    include <OpenCL/opencl.hpp>
+#  else
+#    include <CL/opencl.hpp>
+#  endif
+#elif defined(DLPRIM_USE_CL2_HPP)
 #  ifndef CL_HPP_ENABLE_EXCEPTIONS
 #     define CL_HPP_ENABLE_EXCEPTIONS
 #  endif
